0245a28619598142720908b29d3dbcce32cabb2b
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-09-03 15:10:23 +0200

    Edit: Typo

--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
 ]
 
 # Static and media files location for development
-if settings.DEBUG:
+if os.environ.get('DEBUG') == 'True':
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
