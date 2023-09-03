ddb265d3c5628fc7042e562b0e16c91903a73507
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-09-03 15:30:50 +0200

    Add the main app urls to project's url path

--- config/urls.py
+++ config/urls.py
@@ -2,13 +2,16 @@ import os
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main.urls', namespace='main')),  # main app urls
 ]
 
 # Static and media files location for development
 if os.environ.get('DEBUG') == 'True':
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
