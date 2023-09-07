b5feffbd124064ea3ef63f9802395a704af991e3
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-09-08 01:26:41 +0200

    Register User model to admin site

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """
+    Custom User model admin.
+    """
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'is_active')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    filter_horizontal = ()
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'is_active')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('email', 'password1', 'password2')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'is_active')}),
+    )
