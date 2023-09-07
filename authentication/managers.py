9a2687cbdd213c08e11bacc1c04d298ef33860bf
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-09-07 16:29:11 +0200

    Create custom user model manager

---
+++ authentication/managers.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, first_name, last_name, password=None, **extra_fields):
+        """
+        Create and save a User with the given email, first name, last name, and password.
+        """
+        if not email:
+            raise ValueError('Email must be set')
+        if not first_name:
+            raise ValueError('First Name must be set')
+        if not last_name:
+            raise ValueError('Last Name must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password=None, **extra_fields):
+        """
+        Create and save a SuperUser with the given email, first name, last name, and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_admin', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get('is_admin') is not True:
+            raise ValueError('Superuser must have is_admin=True.')
+        if extra_fields.get('is_active') is not True:
+            raise ValueError('Superuser must have is_active=True.')
+
+        return self.create_user(email, first_name, last_name, password, **extra_fields)
