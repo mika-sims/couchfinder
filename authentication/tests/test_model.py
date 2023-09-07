fd73e9ca198265629471092d4a7e3d51b6000355
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-09-07 23:26:11 +0200

    Write tests for custom user model

---
+++ authentication/tests/test_model.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+class UserTests(TestCase):
+    """
+    Test suite for the custom user model.
+    """
+    def setUp(self):
+        """
+        Test suite setup.
+        """
+        self.user= User.objects.create_user(
+            email='mikailsimsek@mail.com',
+            first_name='Mikail',
+            last_name='Simsek',
+            password='qwerty123',
+        )
+
+    def test_str(self):
+        """
+        Test string representation.
+        """
+        self.assertEqual(str(self.user), 'Mikail Simsek')
