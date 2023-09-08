0f3cf9aa7120a37b9af72a2ece936456c56046f2
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-09-08 12:46:07 +0200

    Edit: Change is_active to True

--- authentication/models.py
+++ authentication/models.py
@@ -17,7 +17,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
 
     objects = UserManager()
