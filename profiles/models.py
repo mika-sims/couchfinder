8ac79efee9e8a601563cd5355aba8fe42cdabf06
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-09-11 23:04:03 +0200

    Create Profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from cloudinary.models import CloudinaryField
+from cities_light.models import Country, Region, City
+from django.urls import reverse
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    Model for storing user profile information
+    """
+    COUCH_FINDER_STATUS_CHOICES = (
+        ('hosting', 'Hosting'),
+        ('travelling', 'Travelling'),
+        ('busy', 'Busy'),
+    )
+
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    image = CloudinaryField('image')
+    bio = models.TextField(blank=True, default='')
+    occupation = models.CharField(max_length=250, blank=True, default='')
+    couch_status = models.CharField(
+        max_length=20, choices=COUCH_FINDER_STATUS_CHOICES, default='busy')
+    country = models.ForeignKey(
+        Country, on_delete=models.SET_NULL, null=True, blank=True)
+    region = models.ForeignKey(
+        Region, on_delete=models.SET_NULL, null=True, blank=True)
+    city = models.ForeignKey(
+        City, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+
+    def get_absolute_url(self):
+        return reverse('profiles:profile-details', kwargs={'pk': self.pk})
+
+    def get_update_url(self):
+        return reverse('profiles:profile-update', kwargs={'pk': self.pk})
+
+    def get_delete_url(self):
+        return reverse('profiles:profile-delete', kwargs={'pk': self.pk})
+
+    def get_profile_status(self):
+        return self.profile_status.replace('_', ' ').capitalize()
