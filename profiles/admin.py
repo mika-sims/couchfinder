a1aee1d65443adbda0922293ea7770734d9cd836
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-09-12 16:22:22 +0200

    Register ProfileDetailView to admin site

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """
+    Custom Profile model admin.
+    """
+    list_display = ('user', 'couch_status')
+    list_filter = ('user',)
+    search_fields = ('user',)
+    ordering = ('user',)
+    filter_horizontal = ()
+
