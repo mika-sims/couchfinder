6b6791d5a81a656a525354f94b26c6746fa75981
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-09-12 16:20:53 +0200

    Create ProfileDetailView view to display user profile details

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.views.generic import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+from profiles.models import Profile
+
+class ProfileDetailView(LoginRequiredMixin, DetailView):
+    """
+    View for displaying the user profile details.
+    """
+    model = Profile
+    template_name = 'profile_details.html'
+    context_object_name = 'profile'
+
+    def get_object(self, queryset=None):
+        # Get the 'pk' parameter from the URL
+        user_pk = self.kwargs.get('pk')
+
+        if self.request.user.pk == user_pk:
+            # If it's the user's own profile, return their own profile
+            return self.request.user.profile
+        else:
+            # If it's another user's profile, return that user's profile
+            user = get_user_model().objects.get(pk=user_pk)
+            return user.profile
