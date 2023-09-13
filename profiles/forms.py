c42d4dc9c155d6ed3ab4e7d7e680e23d078b0510
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-09-13 23:00:08 +0200

    Create ProfileForm form to update user profile

---
+++ profiles/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+
+from profiles.models import Profile
+
+
+class ProfileForm(forms.ModelForm):
+    """
+    Form for updating the user profile details.
+    """
+
+    class Meta:
+        """
+        Meta class for ProfileForm.
+        """
+        model = Profile
+        fields = (
+            'image',
+            'bio',
+            'occupation',
+            'couch_status',
+            'country',
+            'region',
+            'city',
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Add placeholders
+        self.fields['bio'].widget.attrs['placeholder'] = 'Tell us about yourself'
+        self.fields['occupation'].widget.attrs['placeholder'] = 'Your occupation'
