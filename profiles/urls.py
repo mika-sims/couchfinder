5e7e2c650b094bdd6e4ee154d5423fa1f8cd6995
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-09-13 23:01:13 +0200

    Add ProfileUpdateView to url path

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProfileDetailView
+from profiles.views import ProfileDetailView, ProfileUpdateView
 
 app_name = 'profiles'
 
 urlpatterns = [
     path('profile/<int:pk>/', ProfileDetailView.as_view(), name='user-profile'),
+    path('profile-update/<int:pk>/', ProfileUpdateView.as_view(), name='profile-update'),
 ]
