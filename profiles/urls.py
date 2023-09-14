9614485786a57589027821d4be67995cd7613a1e
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-09-14 02:08:17 +0200

    Add upload_image view function to url path

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from profiles.views import ProfileDetailView, ProfileUpdateView
+from profiles.views import ProfileDetailView, ProfileUpdateView, upload_image
 
 app_name = 'profiles'
 
 urlpatterns = [
     path('profile/<int:pk>/', ProfileDetailView.as_view(), name='user-profile'),
     path('profile-update/<int:pk>/', ProfileUpdateView.as_view(), name='profile-update'),
+    path('upload_image/', upload_image, name='upload_image'),
 ]
