f76837a5ea589def3f2f4e4c09194d552603b08c
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-09-14 18:16:36 +0200

    Add CustomPasswordChangeView view to projects urls path

--- config/urls.py
+++ config/urls.py
@@ -4,11 +4,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
+from profiles.views import CustomPasswordChangeView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),  # main app urls
+    path('accounts/password/change/', CustomPasswordChangeView.as_view(), name='change-password'),
     path('accounts/', include('allauth.urls')), # django-allauth urls
-    path('profiles/', include('profiles.urls', namespace='profiles')) # profiles app urls
+    path('profiles/', include('profiles.urls', namespace='profiles')), # profiles app urls
 ]
 
 # Static and media files location for development
