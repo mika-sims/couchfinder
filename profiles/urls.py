16c4726504a9eb3db9ad541b45d21a84b44d8751
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-09-16 23:00:13 +0200

    Add AccountDeactivateView to url path

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from profiles.views import ProfileDetailView, ProfileUpdateView, upload_image
+from . import views
 
 app_name = 'profiles'
 
 urlpatterns = [
-    path('profile/<int:pk>/', ProfileDetailView.as_view(), name='user-profile'),
-    path('profile-update/<int:pk>/', ProfileUpdateView.as_view(), name='profile-update'),
-    path('upload_image/', upload_image, name='upload_image'),
+    path('profile/<int:pk>/', views.ProfileDetailView.as_view(), name='user-profile'),
+    path('profile-update/<int:pk>/',
+         views.ProfileUpdateView.as_view(), name='profile-update'),
+    path('upload_image/', views.upload_image, name='upload_image'),
+    path('account-deactivate/', views.AccountDeactivateView.as_view(),
+         name='account-deactivate'),
 ]
