bb41460fb8ebfc80b9c1ecad525e8aeedfa871ab
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-10-07 11:35:51 +0200

    Register reviews to admin site

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        
+    )
+
+    ordering = ('-created_at',)
+
