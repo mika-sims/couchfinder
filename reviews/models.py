30ed7b680474b5d7b791dfb5bafa7641bd3fde22
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-10-10 00:11:41 +0200

    Add related name to fields

--- reviews/models.py
+++ reviews/models.py
@@ -2,17 +2,20 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from profiles.models import Profile
 
+from .managers import ReviewManager
 
 class Review(models.Model):
     """
     Model for storing user reviews
     """
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, default=1)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='reviews')
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='reviews', default=1)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     likes = models.ManyToManyField(get_user_model(), related_name='review_likes')
+    
+    objects = ReviewManager()
     
     class Meta:
         ordering = ('-created_at',)
