a766f662170aa633222fdcacaed0c9f0b81ec8a1
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-10-10 00:12:05 +0200

    Create ReviewManager

---
+++ reviews/managers.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class ReviewManager(models.Manager):
+    def get_reviews_for_user(self, user):
+        return self.filter(user=user)
+
+    def get_reviews_for_profile(self, profile):
+        return self.filter(profile=profile)
