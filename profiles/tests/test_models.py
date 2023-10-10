762a3b278a424b999026be1a4336eadb366ae165
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-10-10 08:10:55 +0200

    Delete unused files and code

--- profiles/tests/test_models.py
+++ profiles/tests/test_models.py
@@ -1,7 +1,5 @@
 from django.test import TestCase
-from django.urls import reverse
 from django.contrib.auth import get_user_model
-from cities_light.models import Country, Region, City
 
 
 from profiles.views import Profile
